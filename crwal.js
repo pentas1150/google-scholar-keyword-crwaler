e46a5a118dfd7b5e1c3fd4ee3bc081d51b926e58
pentas1150 <beewol@soongsil.ac.kr>
2019-11-25 19:47:21 +0900

    크롤링 완료

---
+++ crwal.js
@@ -0,0 +1,73 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+let paperTitle = [];
+let keywordCount = [];
+
+const replaceAll = (srcStr, searchStr, replaceStr) => {
+    return srcStr.split(searchStr).join(replaceStr);
+}
+
+const getHtml = async(page_number, search_string, start_year) => {
+    try {
+        const start_count = (Number(page_number) - 1) * 10;
+        const search_keyword = replaceAll(search_string, " ", "+");
+        let url = `https://scholar.google.com/scholar?start=${start_count}&q=${search_keyword}`;
+        if(start_year !== 0)
+            url+=`&as_ylo=${start_year}`;
+
+        return await axios.get(url);
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+const getPaperTitle  = async(search, year) => {
+    try {
+        for(let i=1; true; ++i) {
+            console.log(i);
+            const html = await getHtml(i, search, year);
+
+            const $ = cheerio.load(html.data);
+            const $bodylist = $("h3");
+
+            let title_cnt = 0;
+            $bodylist.each(function(idx, elem) {
+                paperTitle.push($(this).find("a").text());
+                title_cnt++;
+            });
+            if(title_cnt === 0)
+                return;
+        }
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+const wordCount = () => {
+    paperTitle.forEach((title) => {
+        title.split(" ").forEach((keyword) => {
+            const isExist = keywordCount.find((elem, idx) => {
+                return elem.keyword === keyword;
+            });
+            if(isExist === undefined) {
+                keywordCount.push({ keyword : keyword, count : 1 });
+            } else {
+                isExist.count++;
+            }
+        });
+    });
+};
+
+const runApp = async() => {
+    await getPaperTitle("energy harvesting sensor", 2015);
+    wordCount();
+    keywordCount.sort((a, b) => {
+        return b.count - a.count;
+    });
+    keywordCount.forEach((elem) => {
+        console.log(elem);
+    });
+};
+
+runApp();
