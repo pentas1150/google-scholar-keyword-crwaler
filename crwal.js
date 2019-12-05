05c310f820e20c22d3d074c4684226dacfb7bae9
pentas1150 <beewol@soongsil.ac.kr>
2019-12-05 17:04:59 +0900

    1.user-agent header 추가 2.터미널에 결과 출력 & 결과 파일에 저장 3.변수명 변경 및 주석

--- crwal.js
+++ crwal.js
@@ -1,32 +1,38 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs');
-require('dotenv').config();
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+require("dotenv").config();
 
 let paperTitle = [];
 let keywordCount = [];
 
 const replaceAll = (srcStr, searchStr, replaceStr) => {
     return srcStr.split(searchStr).join(replaceStr);
-}
+};
 
-const getHtml = async(page_number, search_string, start_year) => {
+const getHtml = async (page_number, search_string, start_year) => {
     try {
         const start_count = (Number(page_number) - 1) * 10;
-        const search_keyword = replaceAll(search_string, " ", "+");
-        let url = `https://scholar.google.com/scholar?start=${start_count}&q=${search_keyword}`;
-        if(start_year !== 0)
-            url+=`&as_ylo=${start_year}`;
+        const parsing_string = replaceAll(search_string, " ", "+");
 
-        return await axios.get(url);
-    } catch(err) {
+        const config = {
+            headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+            }
+        };
+        let url = `https://scholar.google.com/scholar?start=${start_count}&q=${parsing_string}`;
+        if (start_year !== 0) url += `&as_ylo=${start_year}`;
+
+        return await axios.get(url, config);
+    } catch (err) {
         console.error(err);
     }
 };
 
-const getPaperTitle  = async(search, year) => {
+const getPaperTitle = async (search, year) => {
     try {
-        for(let i=1; i<=Number(process.env.END_PAGE); ++i) {
+        for (let i = 1; true; ++i) {
             const html = await getHtml(i, search, year);
 
             const $ = cheerio.load(html.data);
@@ -34,25 +40,32 @@ const getPaperTitle  = async(search, year) => {
 
             let title_cnt = 0;
             $bodylist.each(function(idx, elem) {
-                paperTitle.push($(this).find("a").text());
+                paperTitle.push(
+                    $(this)
+                        .find("a")
+                        .text()
+                );
                 title_cnt++;
             });
-            if(title_cnt === 0)
+            //끝 페이지 판단
+            if (title_cnt === 0) {
+                console.log("end page");
                 return;
+            }
         }
-    } catch(err) {
+    } catch (err) {
         console.error(err);
     }
 };
 
 const wordCount = () => {
-    paperTitle.forEach((title) => {
-        title.split(" ").forEach((keyword) => {
+    paperTitle.forEach(title => {
+        title.split(" ").forEach(keyword => {
             const isExist = keywordCount.find((elem, idx) => {
                 return elem.keyword === keyword;
             });
-            if(isExist === undefined) {
-                keywordCount.push({ keyword : keyword, count : 1 });
+            if (isExist === undefined) {
+                keywordCount.push({ keyword: keyword, count: 1 });
             } else {
                 isExist.count++;
             }
@@ -60,14 +73,19 @@ const wordCount = () => {
     });
 };
 
-const runApp = async() => {
+const runApp = async () => {
     await getPaperTitle(process.env.SEARCH, process.env.START_YEAR);
     wordCount();
     keywordCount.sort((a, b) => {
         return b.count - a.count;
     });
-    keywordCount.forEach((elem) => {
-        fs.writeFileSync('./result.txt', `${elem.keyword} : ${elem.count}`, 'utf8');
+    keywordCount.forEach(elem => {
+        console.log(elem);
+        fs.writeFileSync(
+            "./result.txt",
+            `${elem.keyword},${elem.count}\n`,
+            "utf8"
+        );
     });
 };
 
